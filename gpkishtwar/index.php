d6b002be54257f868e105a7cbb8ee8fdf7bdb3fb
shakeeelz <shakeeelz@gmail.com>
2024-10-10 08:33:21 +0530

    Add files via upload

---
+++ gpkishtwar/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>GPC Kishtwar</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+  </head>
+  <body class="is-preload">
+    <div id="wrapper">
+      
+      <!-- Main -->
+      <div id="main">
+        <div class="inner">
+          
+          <!-- Header -->
+          <?php include 'sections/header.html'; ?>
+          
+          <!-- Banner -->
+          <?php include 'main/banner.html'; ?>
+          
+          <!-- Other sections -->
+          <?php include 'main/section1.html'; ?>
+          <?php include 'main/section2.html'; ?>
+          <?php include 'main/AboutUs.html'; ?>
+          
+        </div>
+      </div>
+      
+      <!-- Sidebar -->
+      <?php include 'sections/sidebar.html'; ?>
+      
+   
+      
+    </div>
+    <div>
+    <?php include 'sections/footer.html'; ?>
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
